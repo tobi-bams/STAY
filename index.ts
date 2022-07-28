2e6fecdbdd78ec0e4c2b5c5a5589876e7c019690
Bamidele Oluwatobi <teebams49@gmail.com>
2022-07-28 13:45:31 +0100

    update: package installation

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// const { sequelize } = require("./models");
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, async () => {
+  try {
+    // await sequelize.authenticate();
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`Server Running at port ${PORT}`);
+});
