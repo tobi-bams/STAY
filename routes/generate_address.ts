278eff5a80983374c30ad870023367235055a0cf
Bamidele Oluwatobi <teebams49@gmail.com>
2022-07-29 12:51:16 +0100

    feat: Authentication middleware

--- routes/generate_address.ts
+++ routes/generate_address.ts
@@ -1,9 +1,10 @@
 import express, { Router, Request, Response } from "express";
 import { GenerateAddress } from "../controller/generate_address";
+import { auth } from "../middleware/auth";
 
 const route: Router = express.Router();
 
-route.post("/generate", async (req: Request, res: Response) => {
+route.post("/generate", auth, async (req: Request, res: Response) => {
   const response = await GenerateAddress(req.body);
   res.status(response.status).json(response.body);
 });
