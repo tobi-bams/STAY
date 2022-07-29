09aeaf61a84173d0da1d29163ac57b27084667b4
Bamidele Oluwatobi <teebams49@gmail.com>
2022-07-29 20:34:21 +0100

    update: webhook handler to update wallet balance and deposit table

--- controller/webhookHandler.ts
+++ controller/webhookHandler.ts
@@ -8,7 +8,27 @@ interface WebHookResponse {
 export const WebhookHandler = async (req: WebHookResponse) => {
   if (req.event === "address.deposit") {
     const rate = await getFiatValue(req.data.coin);
-    const totalValue = rate * req.data.human_readable_amount;
-    console.log(req.data.address_reference);
+    const totalFiatValue = rate * req.data.human_readable_amount;
+    const t = await models.sequelize.transaction();
+    try {
+      const deposit = await models.deposit.findOne({
+        where: { reference: req.data.address_reference },
+        include: models.wallet,
+      });
+      await models.deposit.update(
+        { amount: deposit.amount + totalFiatValue, rate, status: "success" },
+        { where: { reference: req.data.address_reference } },
+        { transaction: t }
+      );
+      await models.wallet.update(
+        { balance: deposit.wallet.balance + totalFiatValue },
+        { where: { id: deposit.walletId } },
+        { transaction: t }
+      );
+      await t.commit();
+    } catch (error) {
+      await t.rollback();
+      console.log(error);
+    }
   }
 };
--- routes/webhook.ts
+++ routes/webhook.ts
@@ -4,7 +4,6 @@ import { WebhookHandler } from "../controller/webhookHandler";
 const route: Router = express.Router();
 
 route.post("/handler", async (req: Request, res: Response) => {
-  console.log(req.body);
   await WebhookHandler(req.body);
   res.status(200).json(req.body);
 });
